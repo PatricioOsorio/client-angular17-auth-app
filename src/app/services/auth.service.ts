1aba92c88b0147df3d69fedf006481c78ad12568
PatricioOsorio <patriciomiguel_12@hotmail.com>
2024-06-23 22:28:08 -0600

    ✨ feat: auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { LoginRequest } from '../interfaces/login-request';
 import { AuthResponse } from '../interfaces/auth-response';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  apiUrl: string = environment.apiUrl
+  apiUrl: string = environment.apiUrl;
+  private tokenKey = 'token';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  login (data : LoginRequest) : Observable<AuthResponse> {
-     
+  login(data: LoginRequest): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(`${this.apiUrl}/account/login`, data).pipe(
+      map((response) => {
+        if (response.isSuccess) {
+          localStorage.setItem(this.tokenKey, response.token);
+        }
+        return response;
+      }),
+    );
   }
 }
