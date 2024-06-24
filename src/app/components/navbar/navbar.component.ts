a2f9f1ea7e22a39a7933011c82c41e860b34a547
PatricioOsorio <patriciomiguel_12@hotmail.com>
2024-06-24 13:58:11 -0600

    ✨ feat: navbar suscribe to router event to update userDetail

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { AfterViewInit, Component, OnChanges, OnInit, SimpleChanges, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbar } from '@angular/material/toolbar';
-import { Router, RouterLink } from '@angular/router';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { MatMenuModule } from '@angular/material/menu';
 import { UserDetail } from '../../interfaces/user-detail';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { Subscription, filter } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -19,17 +20,25 @@ export class NavbarComponent implements OnInit {
   authService = inject(AuthService);
   router = inject(Router);
   matSnackBar = inject(MatSnackBar);
-  userDetail?: UserDetail | null;
 
-  ngOnInit(): void {
-    this.userDetail = this.authService.getUserDetail();
-  }
+  userDetail = this.authService.getUserDetail();
 
-  isLoggedIn = () => this.authService.isLoggedIn();
+  private routerSubscription: Subscription = new Subscription();
+
+  isLoggedIn = (): boolean => this.authService.isLoggedIn();
 
   logout = () => {
     this.authService.logout();
     this.matSnackBar.open('Cerre de sesión correcto', 'Cerrar', { duration: 5000 });
     this.router.navigate(['/login']);
   };
+
+  ngOnInit() {
+    // subscribe to router events to update userDetail
+    this.routerSubscription = this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.userDetail = this.authService.getUserDetail();
+      });
+  }
 }
