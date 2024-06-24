3570c73b42633c1f9908b1983da914d29076a97a
PatricioOsorio <patriciomiguel_12@hotmail.com>
2024-06-23 22:28:18 -0600

    ✨ feat: login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,12 +3,14 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { MatFormField } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatFormField, MatInputModule, MatIcon, ReactiveFormsModule, RouterLink],
+  imports: [MatFormField, MatSnackBarModule, MatInputModule, MatIcon, ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
@@ -17,12 +19,32 @@ export class LoginComponent implements OnInit {
   form!: FormGroup;
   _formBuilder = inject(FormBuilder);
 
+  // injection of services
+  authService = inject(AuthService);
+  matSnackBar = inject(MatSnackBar);
+  router = inject(Router);
+
   ngOnInit(): void {
     this.form = this._formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
+      correo: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
     });
   }
 
-  login() {}
+  login() {
+    this.authService.login(this.form.value).subscribe({
+      next: (response) => {
+        if (response.isSuccess) {
+          this.matSnackBar.open('Inicio de sesión exitoso', 'Cerrar', { duration: 5000 });
+          this.router.navigate(['/']);
+        } else {
+          this.matSnackBar.open('Falló el inicio de sesión', 'Cerrar', { duration: 5000 });
+        }
+      },
+      error: (error) => {
+        const errorMessage = error.error.message || 'error';
+        this.matSnackBar.open(errorMessage, 'Cerrar', { duration: 5000 });
+      },
+    });
+  }
 }
