e6b71825a481b3bf2bdc43714e92185e64ba1aa3
PatricioOsorio <patriciomiguel_12@hotmail.com>
2024-06-26 12:55:38 -0600

    ✨ feat: Display List of users with their roles in client

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,15 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { AsyncPipe } from '@angular/common';
+import { UserDetail } from '../../interfaces/user-detail';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './users.component.html',
-  styles: ``
+  styles: ``,
 })
 export class UsersComponent {
   authService = inject(AuthService);
 
-  
+  users$: Observable<UserDetail[]> = this.authService.getAllUsers();
 }
